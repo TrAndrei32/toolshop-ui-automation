550b0205b0686ad3d496e33e25282919bdfc241c
Andrei Trofin <trofinandrei1@gmail.com>
2026-04-27 22:26:44 +0300

    add wait on page.goto

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -5,7 +5,7 @@ from pages.home_page import HomePage
 class TestHomepage:
     @pytest.mark.smoke
     def test_signin_button(self, page, base_url):
-        page.goto(base_url)
+        page.goto(base_url, wait_until="networkidle")
         signin_button = HomePage(page)
         signin_button.signin()
         assert page.url.endswith("/auth/login")
